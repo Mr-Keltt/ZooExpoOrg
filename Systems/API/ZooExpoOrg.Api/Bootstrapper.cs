faa61e0f217e954b4bc62c6509cd44c5f310fd28
MrKeltt <dmitriydavidenkokeltt@gmail.com>
2024-02-18 11:48:03 +0300

    - Создана базовая структура папок - Создан основной проэкт приложения - Добавленs некоторые общие фалы - Добавлен сервис настроек - Добавлен сервис логирования

---
+++ Systems/API/ZooExpoOrg.Api/Bootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+using ZooExpoOrg.Settings;
+
+namespace ZooExpoOrg.Api;
+
+public class Bootstrapper
+{
+    public static IServiceCollection RegisterServices(IServiceCollection service)
+    {
+        service
+            .AddMainSettings()
+            .AddSwaggerSettings()
+            .AddLogSettings();
+
+        return service;
+    }
+}
