b8cedac07b77ac260e24cad4c5381f1a0d9545d7
mrkeltt <dmitriydavidenkokeltt@gmail.com>
2024-03-22 00:33:25 +0300

    Add photos service

---
+++ Services/ZooExpoOrg.Services.Photos/Bootstrapper.cs
@@ -0,0 +1,12 @@
+﻿namespace ZooExpoOrg.Services.Photos;
+
+using Microsoft.Extensions.DependencyInjection;
+
+public static class Bootstrapper
+{
+    public static IServiceCollection AddPhotoService(this IServiceCollection services)
+    {
+        return services
+            .AddSingleton<IPhotoService, PhotoService>();
+    }
+}
---
+++ Services/ZooExpoOrg.Services.Photos/Photos/IPhotoService.cs
@@ -0,0 +1,9 @@
+﻿namespace ZooExpoOrg.Services.Photos;
+
+public interface IPhotoService
+{
+    public Task<IEnumerable<PhotoModel>> GetAllOwnedById(Guid OwnerId);
+    public Task<PhotoModel> GetById(Guid id);
+    public Task<PhotoModel> Create(CreatePhotoModel model);
+    public Task Delete(Guid id);
+}
